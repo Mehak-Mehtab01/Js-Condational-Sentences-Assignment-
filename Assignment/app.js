d559e5f89de93090b857b8627109963ef74cf154
MEHAK MEHTAB <mehakmehtab01@gmail.com>
2024-10-11 10:50:15 +0500

    Add files via upload

---
+++ Assignment/app.js
@@ -0,0 +1,105 @@
+//HOILDAY ASSIGNMENT (logic building)
+
+//Question no 01 : Write a JavaScript program that asks the user for their age and then displays a message indicating whether they are eligible to vote (18 or older).
+
+// let age = prompt("Please enter your age");
+ 
+// if (age >= 18){
+//     document.write("Congrats! You are eligible to vote")
+// }
+// else{
+//     document.write("Sorry  You are not  eligible to vote")
+// }
+
+//Question no 02 :Use a conditional statement to determine whether a number is positive, negative, or zero.
+
+// let num = prompt("Please enter you number");
+
+// if (num < 0){
+// document.write("It is a negative number ")
+// }
+// else if ( num == 0){
+//     document.write("It is zero")
+// }
+// else {
+//     document.write("It is a positive number")
+// }
+
+//Question no 03 :Write a JavaScript program that asks the user for their favorite color and then displays a message indicating whether they like a primary color (red, blue, or yellow, green).
+
+// let color = prompt("Please enter your favourite color?");
+ 
+// if(color == "red"  || color == "green" || color == "blue" || color == "yellow"){
+//     document.write("You like primary color");
+// }
+// else{
+//     document.write("Your favourite color is not a primary color");
+// }
+
+//Question no 04 : Use logical operators to determine whether a number is between 10 and 20 (use logical operator)
+
+// let num = prompt("Please enter your number");
+
+// if (num >= 10 && num <= 20){
+//     document.write("Your number is between 10 and 20")
+// }
+// else{
+//     document.write("Your number is not between 10 and 20")
+// }
+
+//Question no 05 : Write a JavaScript program that asks the user for their grade (A, B, C, D, or F) and then displays a message indicating whether they passed or failed.
+ 
+// let marks = prompt("Please Enter Your Number");
+
+// if(marks >= 80 && marks <=100){
+//     document.write("congrats! You have got A+ Grade")
+// }
+// else if(marks >= 70 && marks <=79  ){
+//     document.write("congrats! You have got A Grade")
+// }
+// else if(marks >= 60 && marks <=69  ){
+//     document.write("congrats! You have got B Grade")
+// }
+// else if(marks >= 50 && marks <=59  ){
+//     document.write("congrats! You have got C Grade")
+// }
+// else if( marks > 100){
+//     document.write("This number is not valid")
+// }
+// else {
+//     document.write("You are Fail loser")
+// }
+
+//Question no 06 : Write a JavaScript program that asks the user for their age and gender, and then displays a message indicating whether they are eligible to serve in the military (18-30 years old, male).
+
+// let age = prompt("Please Enter your age");
+// let gender = prompt("Please enter your gender");
+
+// if ( age >= 18 && age <=30 && gender == "male"){
+//     document.write("Congrats! Your are eligible to serve in the military ");
+// }
+// else{
+//     document.write("Sorry! Your are  not  eligible to serve in the military ");
+// }
+
+
+//Question no 07 :. Use logical operators to determine whether a number is even or odd.
+ 
+// let num = prompt("Please enter your number");
+// if(num % 2 == 0 ){
+//     document.write("it is even number");
+// }
+// else{
+//     document.write("it is odd number");
+// }
+
+//Question no 08 : Use a conditional statement to determine whether a number is a multiple of 3.
+
+//  let multiple = prompt("Please enter your number");
+
+//  if (multiple % 3 == 0){
+//     document.write(multiple , " is a multiple of  3");
+//  }
+//  else{
+//     document.write(multiple, " is  not a  multiple of  3");
+//  }
